5a60a716cb86a1801640007acb02c97406d4880f
acorn <w1013d27@126.com>
2023-08-05 19:49:22 +0800

    v1.6.4

--- hello.go
+++ hello.go
@@ -1,8 +1,16 @@
 package helloworld
 
-import "github.com/newacorn/dog/v2"
+import (
+	"github.com/Jemmic/go-pcre2"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/newacorn/dog/v2"
+)
 
 func Hello() {
 	println("v2")
 	dog.Bulk()
+	println(pcre2.ErrInvalidRegexp)
+	v := goquery.Selection{}
+	println(&v)
+
 }
